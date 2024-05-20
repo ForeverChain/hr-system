3b86993808baf02ee21cdf429c4c44cead86ed9e
monherdene <interactivemonh@gmail.com>
2024-05-20 15:55:14 +0800

    changes of type

--- backend/models/Candidate.js
+++ backend/models/Candidate.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
 
 const candidateSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  education: [{ type: mongoose.Schema.Types.ObjectId, ref: "Education" }],
-  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
-  foreignLanguages: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "ForeignLanguage" },
-  ],
-  workExperiences: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "WorkExperience" },
-  ],
-  type: { type: String, required: true, default: "pending" },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    education: [{ type: mongoose.Schema.Types.ObjectId, ref: "Education" }],
+    courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+    foreignLanguages: [{ type: mongoose.Schema.Types.ObjectId, ref: "ForeignLanguage" }],
+    workExperiences: [{ type: mongoose.Schema.Types.ObjectId, ref: "WorkExperience" }],
+    type: { type: String, required: true, default: "pending" },
+    interviewTime: { type: String, required: false },
 });
 
 const CandidateModel = mongoose.model("Candidate", candidateSchema);
